3142fdd44032b3f9009bc20694db3188b2ac21a1
DovahkiinH <loick.caplier@gmail.com>
2024-01-23 14:44:43 +0100

    Record

--- index.php
+++ index.php
@@ -1,54 +1,67 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste des enregistrements</title>
+    <title>Liste des Disques</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/Assets/Css/Css.css">
 </head>
+
 <body>
-
-    <h2>Liste des enregistrements dans la table "disc"</h2>
-
     <?php
-    // Connexion à la base de données (à adapter selon votre configuration)
-    $conn = new mysqli('localhost', 'admin', 'Afpa1234', 'disc');
 
-    // Vérification de la connexion
-    if ($conn->connect_error) {
-        die("La connexion a échoué : " . $conn->connect_error);
-    }
+        $servername = "localhost";
+        $username = "admin";
+        $password = "Afpa1234";
+        $dbname = "record";
 
-    // Requête pour récupérer les enregistrements de la table "disc"
-    $query = "SELECT disc_id , disc_title , disc_year , disc_picture , disc_label , disc_genre , disc_price , disc.artist_id , artist.artist_id , artist_name FROM disc , artist WHERE disc.artist_id = artist.artist_id";
-    $result = $conn->query($query);
-
-    // Vérification des résultats
-    if ($result->num_rows > 0) {
-        // Affichage des enregistrements dans un tableau HTML
-        echo '<table border="1">';
-        echo '<tr><th>ID</th><th>Title</th><th>Artist</th><th>Year</th></tr>';
-
-        while ($row = $result->fetch_assoc()) {
-            echo '<tr>';
-            echo '<td>' . $row['disc_id'] . '</td>';
-            echo '<td>' . $row['disc_title'] . '</td>';
-            echo '<td>' . $row['disc_year'] . '</td>';
-            echo '<td>' . $row['disc_picture'] . '</td>';
-            echo '<td>' . $row['disc_label'] . '</td>';
-            echo '<td>' . $row['disc_genre'] . '</td>';
-            echo '<td>' . $row['disc_price'] . '</td>';
-            echo '<td>' . $row['artist_name'] . '</td>';
-            echo '</tr>';
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("La connexion à la base de données a échoué : " . $e->getMessage());
         }
 
-        echo '</table>';
-    } else {
-        echo "Aucun enregistrement trouvé dans la table 'disc'.";
+
+    $sql = "SELECT disc_id, disc_title, disc_year, disc_picture, disc_genre, disc_label FROM disc";
+
+    try {
+        $result = $conn->query($sql);
+
+
+        if ($result->rowCount() > 0) {
+            $count = 0;
+            echo '<h1>Liste des Disques (' . $result->rowCount() . ' disques)</h1>';
+            echo '<a class="btn btn-primary" href="add_form.php">Ajouter</a><hr>';
+            echo '<div class="disque-container">';
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                echo '<div class="disque">';
+                echo '<img src="assets/img/' . $row['disc_picture'] . '" alt="Image du disque">';
+                echo '<div class="details">';
+                echo '<b>' . $row['disc_title'] . '</b>';
+                echo '<p>Année : ' . $row['disc_year'] . '</p>';
+                echo '<p>Genre : ' . $row['disc_genre'] . '</p>';
+                echo '<p>Label : ' . $row['disc_label'] . '</p>';
+                echo '<a class="btn btn-primary" href="details.php?disc_id=' . $row['disc_id'] . '">Détail</a>';
+                echo '</div>';
+                echo '</div>';
+                $count++;
+            }
+            echo '</div>';
+        } else {
+            echo '<h1>Liste des Disques (0 disque)</h1>';
+            echo "Aucun enregistrement trouvé dans la table 'disc'.";
+        }
+    } catch (PDOException $e) {
+        echo "Erreur de requête :" . $e->getMessage();
     }
 
-    // Fermeture de la connexion à la base de données
-    $conn->close();
+    $conn = null;
     ?>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
